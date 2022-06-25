b8bf365e1237244faa3f459356d15bf3d2298300
Erna Lukac <e.lukac4@gmail.com>
2022-06-25 17:18:38 -0600

    final format for nav bar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,54 +1,35 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
+import { useState } from 'react';
+import './nav.css';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import ArticleIcon from '@mui/icons-material/Article';
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import pdf from '../../assets/ELukacResume.pdf';
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
 
-export default function navBar() {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div role="presentation" onClick={handleClick}>
-      <Breadcrumbs aria-label="breadcrumb">
-      <Typography
-          sx={{ display: 'flex', alignItems: 'center' }}
-        >
-          <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-        Home
-        </Typography>
-
-        <Link
-          underline="hover"
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="#work"
-        >
-          <AccountTreeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-        Work
-        </Link>
-        <Link
-          underline="hover"
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="/material-ui/getting-started/installation/"
-        >
-          <ArticleIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-        Skills
-        </Link>
-        <Link
-          underline="hover"
-          sx={{ display: 'flex', alignItems: 'center' }}
-          href="#contact"
-        >
-            <ConnectWithoutContactIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-         Contact
-        </Link>
-      </Breadcrumbs>
-
-    </div>
-  );
+      <nav>
+          <a href="#home"
+              onClick={() => setActiveNav('#avatarbox')}
+              className={activeNav === '#avatarbox' ? 'active' : ''}
+          ><HomeIcon/>Home</a>
+          <a href="#mywork"
+              onClick={() => setActiveNav('#mywork')}
+              className={activeNav === '#mywork' ? 'active' : ''}
+          ><AccountTreeIcon/>Work</a>
+          <a href="{pdf}" target="_blank" rel="noopener"
+              onClick={() => setActiveNav('#work')}
+              className={activeNav === '#work' ? 'active' : ''}
+          ><ArticleIcon/> Skills</a>
+          <a href="#contact"
+              onClick={() => setActiveNav('#contact')}
+              className={activeNav === '#contact' ? 'active' : ''}
+          ><ConnectWithoutContactIcon/> Contact </a>
+      </nav>
+  )
 }
+
+export default Nav;
