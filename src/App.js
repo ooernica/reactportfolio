2121f97e16362735ed1697dc02d617982f47a692
Erna Lukac <e.lukac4@gmail.com>
2022-06-25 17:43:35 -0600

    final changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 
-
 function App() {
   return (
     <div>
